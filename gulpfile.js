6f8f7b6f94feb754ddef8fcae1dee450f06b9f2d
Lukáš Matta <445679@mail.muni.cz>
2018-06-19 20:44:39 +0200

    Add javascript build

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,11 @@ gulp.task('compress', function() {
     return gulp.src("src/**/*.js")
         .pipe(closureCompiler({
             fileName: 'cbf.min.js',
+            continueWithWarnings: true,
             compilerFlags: {compilation_level: 'SIMPLE_OPTIMIZATIONS'}
         }))
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest('dist'))
+        .on('error', console.error.bind(console));
 });
 
 gulp.task("default", ["compress"], function() {});
